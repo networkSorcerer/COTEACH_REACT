1a44a7189fdfd8a919e498e13bd577dc32ac9c9f
networkSocerer <gigattfyhig@gmail.com>
2025-09-20 12:57:42 +0900

    react 화이팅

---
+++ first-app/src/component/BoxClass.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class BoxClass extends Component {
+  componentWillUnmount() {
+    console.log("bye bye");
+  }
+  render() {
+    return <div>Box{this.props.num}</div>;
+  }
+}
