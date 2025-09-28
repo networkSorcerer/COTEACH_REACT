e92be7582b4d1fd78f5022c819b4fcf4bd40a59b
networkSorcerer <gigattfyhig@gmail.com>
2025-09-28 20:20:38 +0900

    react 화이팅git add .git add .!

--- hnm-router/src/App.jsx
+++ hnm-router/src/App.jsx
@@ -3,9 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import ProductAll from "./page/ProductAll";
 import Login from "./page/Login";
 import ProductDetail from "./page/ProductDetail";
+import Navbar from "./component/Navbar";
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="" element={<ProductAll />} />
         <Route path="/login" element={<Login />} />
--- hnm-router/src/component/Navbar.jsx
+++ hnm-router/src/component/Navbar.jsx
@@ -1,11 +1,44 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
+  const menuList = [
+    "여성",
+    "Divided",
+    "남성",
+    "신생아/유아",
+    "아동",
+    "H&M Home",
+    "Sale",
+    "지속가능성",
+  ];
   return (
     <div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>
+        <div className="login-button">
+          <FontAwesomeIcon icon={faUser} />
+          <div>로그인</div>
+        </div>
+      </div>
+      <div className="nav-section">
+        <img
+          width={100}
+          src="https://i.pinimg.com/736x/94/8c/7f/948c7f31df5eb63955907221228842d7.jpg"
+        ></img>
+      </div>
+      <div className="menu-area">
+        <ul className="menu-list">
+          {menuList.map((menu) => (
+            <li>{menu}</li>
+          ))}
+        </ul>
+
+        <div>
+          <FontAwesomeIcon icon={faSearch} />
+          <input type="text" />
+        </div>
+      </div>
     </div>
   );
 };
