f15326d7f264b60695079101e8e0d988834dfc6d
networkSorcerer <gigattfyhig@gmail.com>
2025-09-30 18:54:11 +0900

    취업은 참으로 어렵도다

--- hnm-router/src/component/Navbar.jsx
+++ hnm-router/src/component/Navbar.jsx
@@ -29,8 +29,8 @@ const Navbar = () => {
       </div>
       <div className="menu-area">
         <ul className="menu-list">
-          {menuList.map((menu) => (
-            <li>{menu}</li>
+          {menuList.map((menu, index) => (
+            <li key={index}>{menu}</li>
           ))}
         </ul>
 
--- hnm-router/src/page/ProductAll.jsx
+++ hnm-router/src/page/ProductAll.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ProductCard from "../component/ProductCard";
+import { Col, Container, Row } from "react-bootstrap";
 
 const ProductAll = () => {
-  return <div>ProductAll</div>;
+  const [productList, setProductList] = useState([]);
+  const getProducts = async () => {
+    let url = "http://localhost:5000/products";
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log("dddd", data);
+    setProductList(data);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
+  return (
+    <div>
+      <Container>
+        {productList.map((menu) => (
+          <Col lg={3} key={menu.id}>
+            <ProductCard item={menu} />
+          </Col>
+        ))}
+      </Container>
+    </div>
+  );
 };
 
 export default ProductAll;
