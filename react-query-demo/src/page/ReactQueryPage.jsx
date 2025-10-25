9fde917004e30c6eb95a61cac7376c1d11c0ec4a
networkSorcerer <gigattfyhig@gmail.com>
2025-10-25 20:03:38 +0900

    react 화이팅

--- react-query-demo/src/page/ReactQueryPage.jsx
+++ react-query-demo/src/page/ReactQueryPage.jsx
@@ -4,14 +4,31 @@ import React from "react";
 
 const ReactQueryPage = () => {
   const fetchPost = () => {
-    return axios.get("http://localhost:3004/post");
+    return axios.get("http://localhost:3004/posts");
   };
   const { isLoading, data, isError, error } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchPost,
+    retry: 1,
+    select: (data) => {
+      return data.data;
+    },
   });
   console.log("ddd", data);
-  return <div>ReactQueryPage</div>;
+  console.log("error", error);
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+  return (
+    <div>
+      {data.map((item) => (
+        <div>{item.title}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ReactQueryPage;
